6ab30e669537f6ff21b5e2617c2c1209410732f7
Dennis <bereznd1@tcnj.edu>
2018-03-18 15:29:20 -0400

    Setting up visual structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDaJLu_MeIlC9Ui49BHBSxSV6naduliDQE",
+    authDomain: "train-scheduler-2b826.firebaseapp.com",
+    databaseURL: "https://train-scheduler-2b826.firebaseio.com",
+    projectId: "train-scheduler-2b826",
+    storageBucket: "train-scheduler-2b826.appspot.com",
+    messagingSenderId: "152849792041"
+  };
+  firebase.initializeApp(config);
